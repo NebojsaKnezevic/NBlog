d49bf5e9cf1f535beba47e5ee7b69fe157780c86
Nebojsa Knezevic <global021man@gmail.com>
2025-02-12 21:40:04 +0100

    first commit

---
+++ nblog/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React, { JSX } from "react";
+
+const Dashboard = (): React.ReactElement => {
+    return (
+        <div>
+            <h1>DASHBOARD</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
