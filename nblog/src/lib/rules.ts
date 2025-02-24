385632d58ca0a686486f2f6404cd81d33c4aa5ea
Nebojsa Knezevic <global021man@gmail.com>
2025-02-24 12:27:20 +0100

    add login system

--- nblog/src/lib/rules.ts
+++ nblog/src/lib/rules.ts
@@ -25,3 +25,15 @@ export const RegisterFormSchema = z
         }
     });
 
+
+export const LoginFormSchema = z
+.object({
+    email: z.string().email({ message: "Please enter a valid email." }).trim(),
+    password: z
+        .string()
+        .min(1, { message: "Password cannot be empty." })
+        .min(5, { message: "Password must be at least 5 characters long." }) 
+        .trim(),
+    // server: z.string()
+})
+
